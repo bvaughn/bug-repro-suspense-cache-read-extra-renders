b6e1571c252f95497a98bcfcfd2c265516a4409e
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-04-13 10:01:58 -0400

    Add second cache read

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { Suspense, useState } from "react";
 
 import { createSingleEntryCache } from "suspense";
 
-const dummyCache = createSingleEntryCache({
-  debugLabel: "Dummy",
+const dummyCacheOne = createSingleEntryCache({
   load: () => {
-    console.log("dummyCache.load()");
+    console.log("dummyCacheOne.load()");
+    return "Static value";
+  },
+});
+
+const dummyCacheTwo = createSingleEntryCache({
+  load: async () => {
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    console.log("dummyCacheTwo.load()");
     return "Static value";
   },
 });
@@ -13,13 +20,42 @@ const dummyCache = createSingleEntryCache({
 export function App() {
   let value;
   try {
-    console.log("App: reading...");
-    value = dummyCache.read();
+    console.log("<App> reading...");
+    value = dummyCacheOne.read();
   } catch (error) {
-    console.log("App: caught:", error);
+    console.log("<App> caught:", error);
     throw error;
   }
-  console.log("App: loaded:", value);
+  console.log("<App> loaded:", value);
 
-  return <h1>Hello world!</h1>;
+  return (
+    <div>
+      <div>App: {value}</div>
+      <Child />
+    </div>
+  );
+}
+
+function Child() {
+  const [bool, setBool] = useState(false);
+  return (
+    <>
+      <button onClick={() => setBool(true)}>Toggle</button>
+      <Suspense>{bool && <Loader />}</Suspense>
+    </>
+  );
+}
+
+function Loader() {
+  let value;
+  try {
+    console.log("<Child> reading...");
+    value = dummyCacheTwo.read();
+  } catch (error) {
+    console.log("<Child> caught:", error);
+    throw error;
+  }
+  console.log("<Child> loaded:", value);
+
+  return <div>Loader: {value}</div>;
 }
