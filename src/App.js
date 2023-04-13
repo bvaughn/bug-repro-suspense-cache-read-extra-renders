de506137b4095df84d5563381ff7cf2e742a1f60
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-04-13 10:01:44 -0400

    Add minimum repro attempt based on Holger's replay

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
+import React from "react";
+
+import { createSingleEntryCache } from "suspense";
+
+const dummyCache = createSingleEntryCache({
+  debugLabel: "Dummy",
+  load: () => {
+    console.log("dummyCache.load()");
+    return "Static value";
+  },
+});
+
 export function App() {
+  let value;
+  try {
+    console.log("App: reading...");
+    value = dummyCache.read();
+  } catch (error) {
+    console.log("App: caught:", error);
+    throw error;
+  }
+  console.log("App: loaded:", value);
+
   return <h1>Hello world!</h1>;
 }
